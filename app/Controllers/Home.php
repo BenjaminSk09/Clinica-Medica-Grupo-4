1916828eaaab8820fcb3566b16f9a47e31a856cb
BenjaminSk09 <159175209+BenjaminSk09@users.noreply.github.com>
2024-09-10 10:11:25 -0600

    Datos paciente

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('Vista_paciente.php');
+        return view('form_agregar_paciente.php');
     }
 }
